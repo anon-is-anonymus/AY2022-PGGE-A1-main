8a0f14ae0ea1bb7bb47bfc40ba8224f997d78800
anon-is-anonymus <cylee301203@gmail.com>
2022-01-24 05:55:42 +0800

    Initial commit

---
+++ Assignment1_UnityProject/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+	[SerializeField]
+	private AudioClip[] concreteClips;
+	[SerializeField]
+	private AudioClip[] dirtClips;
+	[SerializeField]
+	private AudioClip[] metalClips;
+	[SerializeField]
+	private AudioClip[] sandClips;
+	[SerializeField]
+	private AudioClip[] woodClips;
+
+	public AudioSource audioSource;
+	public CharacterController characterController;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	void Step()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, 3))
+		{
+			if (hit.transform.gameObject.layer == 9) //Check if ground is concrete
+			{
+				AudioClip clip = concreteClips[Random.Range(0, concreteClips.Length)];
+				audioSource.volume = Random.Range(0.2f, 1); //Randomise loudness of footsteps
+				audioSource.PlayOneShot(clip);
+				Debug.Log("Playing");
+			}
+			if (hit.transform.gameObject.layer == 10) //Check if ground is dirt
+			{
+				AudioClip clip = dirtClips[Random.Range(0, dirtClips.Length)];
+				audioSource.volume = Random.Range(0.2f, 1); //Randomise loudness of footsteps
+				audioSource.PlayOneShot(clip);
+			}
+			if (hit.transform.gameObject.layer == 11) //Check if ground is metal
+			{
+				AudioClip clip = metalClips[Random.Range(0, metalClips.Length)];
+				audioSource.volume = Random.Range(0.2f, 1); //Randomise loudness of footsteps
+				audioSource.PlayOneShot(clip);
+			}
+			if (hit.transform.gameObject.layer == 12) //Check if ground is sand
+			{
+				AudioClip clip = sandClips[Random.Range(0, sandClips.Length)];
+				audioSource.volume = Random.Range(0.2f, 1); //Randomise loudness of footsteps
+				audioSource.PlayOneShot(clip);
+			}
+			if (hit.transform.gameObject.layer == 13) //Check if ground is wood
+			{
+				AudioClip clip = woodClips[Random.Range(0, woodClips.Length)];
+				audioSource.volume = Random.Range(0.2f, 1); //Randomise loudness of footsteps
+				audioSource.PlayOneShot(clip);
+			}
+		}
+	}    
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
---
+++ Assignment1_UnityProject/Assets/Scripts/TPCBase.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PGGE
+{
+	// The base class for all third-person camera controllers
+	public abstract class TPCBase
+	{
+		protected Transform mCameraTransform;
+		protected Transform mPlayerTransform;
+
+		public Transform CameraTransform
+		{
+			get
+			{
+				return mCameraTransform;
+			}
+		}
+		public Transform PlayerTransform
+		{
+			get
+			{
+				return mPlayerTransform;
+			}
+		}
+
+		public TPCBase(Transform cameraTransform, Transform playerTransform)
+		{
+			mCameraTransform = cameraTransform;
+			mPlayerTransform = playerTransform;
+		}
+
+		public void RepositionCamera()
+		{
+			float distance;
+			//minDistance set to 0 so if the player is straight in front of the wall,
+			//the camera will never clip through the wall
+			float minDistance = 0f;
+			float maxDistance = 4f;
+			//dollyDir used to get the direction the cam is facing, normalized to get a 1 value
+			//the 1 value is then used to calculate desiredCameraPos, using the 1 value * max dist,
+			//we can get the ideal camera position as it would face the same direction only the distance changes
+			Vector3 dollyDir = mCameraTransform.localPosition.normalized;
+			//TransformPoint() used as world position is needed to see where in the world the raycast should end
+			Vector3 desiredCameraPos = mCameraTransform.TransformPoint(dollyDir * maxDistance);
+			RaycastHit hit;
+
+			if (Physics.Linecast(mCameraTransform.transform.position, desiredCameraPos, out hit))
+			{
+				//MathF.Clamp() used to ensure that camera will never go past maxDistance or minDistance
+				distance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+			}
+			else
+			{
+				distance = maxDistance;
+			}
+
+			//lerp used to make sure shift is smooth, same with deltatime
+			//deltatime is multiplied by 4 to ensure that the camera wil not phase through objects
+			mCameraTransform.localPosition = Vector3.Lerp(mCameraTransform.localPosition, dollyDir * distance, Time.deltaTime * 4);
+		}
+
+		public abstract void Update();
+	}
+}
+
